2111eb2ffbd909fdf441b19cfac978d8752f00eb
MrPuppeteer <nuriksabagas@gmail.com>
2021-10-30 22:04:22 +0700

    add boilerplate for reuse purpose, etc

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const ejsMate = require('ejs-mate');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.engine('ejs', ejsMate);
 
 //static assets
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -13,8 +16,12 @@ app.use('/public/js', express.static(path.join(__dirname, '/node_modules/bootstr
 
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index');
 });
+
+app.get('/about', (req, res) => {
+	res.render('about');
+})
 
 app.listen(8080, () => {
 	console.log('Serving on port 8080')

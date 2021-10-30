78d95bd5d228aa4b1f60611ad3620d95de8759dc
MrPuppeteer <nuriksabagas@gmail.com>
2021-10-30 20:39:07 +0700

    styling homepage

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 //static assets
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/public/styles', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/public/scripts', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/public/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/public/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 
 
 app.get('/', (req, res) => {

00c6fb1b435331aa10ca433b0c74be61faddd96a
MrPuppeteer <nuriksabagas@gmail.com>
2022-02-06 13:51:09 +0700

    format code with prettier

--- app.js
+++ app.js
@@ -13,10 +13,18 @@ app.engine('ejs', ejsMate);
 
 //static assets
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/public/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/public/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-app.use('/public/css', express.static(path.join(__dirname, '/node_modules/bootstrap-icons/font')));
-
+app.use(
+	'/public/css',
+	express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css'))
+);
+app.use(
+	'/public/js',
+	express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js'))
+);
+app.use(
+	'/public/css',
+	express.static(path.join(__dirname, '/node_modules/bootstrap-icons/font'))
+);
 
 app.get('/', (req, res) => {
 	res.render('index');
@@ -32,11 +40,13 @@ app.get('/contact', (req, res) => {
 
 app.get('/blog', async (req, res, next) => {
 	try {
-	const { data } = await axios.get('https://dev.to/api/articles?username=bagasn');
-	res.render('blog', {data});
+		const { data } = await axios.get(
+			'https://dev.to/api/articles?username=bagasn'
+		);
+		res.render('blog', { data });
 	} catch (e) {
-	// console.error(e);
-	next(new ExpressError('Internal Server Error', 500));
+		// console.error(e);
+		next(new ExpressError('Internal Server Error', 500));
 	}
 });
 
@@ -45,11 +55,11 @@ app.all('*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-	const {statusCode = 500} = err;
+	const { statusCode = 500 } = err;
 	if (!err.message) err.message = 'Something went wrong';
-	res.status(statusCode).render('error', {err});
+	res.status(statusCode).render('error', { err });
 });
 
-app.listen( process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 8080, () => {
 	console.log('Serving on port 8080');
 });
